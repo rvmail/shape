ec39444305be66dd303b5153f3b0c4a3aee13c43
Administrator <Administrator@USER-20160924EH>
2017-02-16 10:59:53 +0800

    添加了第二个界面别人的demo

--- src/com/example/shape/MainActivity.java
+++ src/com/example/shape/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.shape;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button bt1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		bt1=(Button)findViewById(R.id.button1);
+		bt1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
